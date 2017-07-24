68170111e6da864ccf990a59a871e6c9d3403821
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-07-25 08:11:06 +1200

    Adding the scripts for verifying the nomad installation
    references #3

---
+++ tests/pester/Nomad.Tests.ps1
@@ -0,0 +1,18 @@
+Describe 'The nomad application' {
+    Context 'is installed' {
+        # Verify that nomad is installed
+    }
+
+    Context 'has been daemonized' {
+        # Verify that the nomad daemon is configured and is running
+    }
+
+    Context 'can be contacted' {
+        # Verify nomad ports?
+        # Verify that nomad is reachable from the outside
+    }
+
+    Context 'has linked to consul' {
+
+    }
+}
