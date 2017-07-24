dee21b7a38f7387946bc2b85b1682af29c2bc27e
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-07-25 08:10:50 +1200

    Adding the scripts for verifying the consul installation
    references #2

---
+++ tests/pester/Consul.Tests.ps1
@@ -0,0 +1,14 @@
+Describe 'The consul application' {
+    Context 'is installed' {
+        # Verify that consul is installed
+    }
+
+    Context 'has been daemonized' {
+        # Verify that the consul daemon is configured and is running
+    }
+
+    Context 'can be contacted' {
+        # Verify consul ports?
+        # Verify that consul is reachable from the outside
+    }
+}
