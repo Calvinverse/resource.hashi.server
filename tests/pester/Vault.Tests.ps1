30a32e474d6cf7fcfd6fb0cd411242131067e452
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-07-25 08:11:23 +1200

    Adding the script for verifying the vault configuration
    references #4

---
+++ tests/pester/Vault.Tests.ps1
@@ -0,0 +1,18 @@
+Describe 'The vault application' {
+    Context 'is installed' {
+        # Verify that vault is installed
+    }
+
+    Context 'has been daemonized' {
+        # Verify that the vault daemon is configured and is running
+    }
+
+    Context 'can be contacted' {
+        # Verify vault ports?
+        # Verify that vault is reachable from the outside
+    }
+
+    Context 'has linked to consul' {
+
+    }
+}
