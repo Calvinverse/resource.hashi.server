51e4a1909745025d4eaab29b5302d2c2548482e3
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-08-22 22:24:25 +1200

    Updating the tests for nomad
    references #3

--- tests/integration/pester/Nomad.Tests.ps1
+++ tests/integration/pester/Nomad.Tests.ps1
@@ -1,10 +1,46 @@
 Describe 'The nomad application' {
     Context 'is installed' {
-        # Verify that nomad is installed
+        It 'with binaries in /usr/local/bin' {
+            '/usr/local/bin/nomad' | Should Exist
+        }
+
+        It 'with default configuration in /etc/nomad-conf.d/server.hcl' {
+            '/etc/nomad-conf.d/server.hcl' | Should Exist
+        }
+
+        It 'with environment configuration in /etc/nomad-conf.d' {
+            '/etc/nomad-conf.d/bootstrap.hcl' | Should Exist
+            '/etc/nomad-conf.d/connections.hcl' | Should Exist
+            '/etc/nomad-conf.d/region.hcl' | Should Exist
+            '/etc/nomad-conf.d/secrets.json' | Should Exist
+        }
     }
 
     Context 'has been daemonized' {
-        # Verify that the nomad daemon is configured and is running
+        It 'with a systemd service' {
+            '/etc/systemd/system/nomad.service' | Should Exist
+
+            <#
+
+            [Unit]
+            Description=Nomad System Scheduler
+            Requires=network-online.target
+            After=network-online.target
+            Documentation=https://nomadproject.io/docs/index.html
+
+            [Install]
+            WantedBy=multi-user.target
+
+            [Service]
+            ExecStart=/usr/local/bin/nomad agent -config=/etc/nomad-conf.d
+            Restart=on-failure
+
+            #>
+        }
+
+        It 'and is running' {
+
+        }
     }
 
     Context 'can be contacted' {
@@ -13,6 +49,6 @@ Describe 'The nomad application' {
     }
 
     Context 'has linked to consul' {
-
+        # check that consul has the service
     }
 }
