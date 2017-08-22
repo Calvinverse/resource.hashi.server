3d2a5120f304e4ee4ba98a980639b4ad49eaddeb
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-08-22 22:25:50 +1200

    Updating the tests for vault
    references #4

--- tests/integration/pester/Vault.Tests.ps1
+++ tests/integration/pester/Vault.Tests.ps1
@@ -1,10 +1,44 @@
 Describe 'The vault application' {
     Context 'is installed' {
-        # Verify that vault is installed
+        It 'with binaries in /usr/local/bin' {
+            '/usr/local/bin/nomad' | Should Exist
+        }
+
+        It 'with default configuration in /etc/vault/server.hcl' {
+            '/etc/vault/server.hcl' | Should Exist
+        }
+
+        It 'with environment configuration in /etc/vault/conf.d' {
+            '/etc/vault/conf.d/connections.hcl' | Should Exist
+            '/etc/vault/conf.d/region.hcl' | Should Exist
+        }
     }
 
     Context 'has been daemonized' {
-        # Verify that the vault daemon is configured and is running
+        It 'with a systemd service' {
+            '/etc/systemd/system/vault.service' | Should Exist
+
+            <#
+
+            [Unit]
+            Description=Vault
+            Requires=network-online.target
+            After=network-online.target
+            Documentation=https://vaultproject.io
+
+            [Install]
+            WantedBy=multi-user.target
+
+            [Service]
+            ExecStart=/usr/local/bin/vault server -config=/etc/vault/server.hcl -config=/etc/vault/conf.d
+            Restart=on-failure
+
+            #>
+        }
+
+        It 'and is running' {
+
+        }
     }
 
     Context 'can be contacted' {
